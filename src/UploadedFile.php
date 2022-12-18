d73901e91b15794169b9256177a7d10522f1a737
Rajkumar Dusad <32831684+rajkumardusad@users.noreply.github.com>
2022-12-18 14:57:24 +0530

    Create UploadedFile.php

---
+++ src/UploadedFile.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Unic;
+
+class UploadedFile
+{
+    public $name = null;
+    public $type = null;
+    public $tmpName = null;
+    public $size = null;
+    public $error = null;
+
+    public function __constructor(array &$file) {
+        $this->name = $file['name'] ?? null;
+        $this->type = $file['type'] ?? null;
+        $this->tmpName = $file['tmp_name'] ?? null;
+        $this->size = $file['size'] ?? null;
+        $this->error = $file['error'] ?? null;
+    }
+
+    /**
+     * Upload files on the server.
+     *
+     * @param string $path
+     * @param string $filename
+     * @return bool
+     */
+    public function save(string $path, string $fileName = null)
+    {
+        if ($fileName != null) {
+            $path = dirname($path) . '/' . $fileName;
+        }
+        return move_uploaded_file($this->tmpName, $path);
+    }
+}
